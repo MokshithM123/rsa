4e8a9671427d2726542638f08a5d761afb75963e
labs34 <131676255+labs34@users.noreply.github.com>
2023-07-06 18:58:31 +0530

    Update rsa.c

--- rsa.c
+++ rsa.c
@@ -1,55 +1,93 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
-int gcd(int m,int n)
-{
-int temp=m%n;
-if(temp==0)
- return n;
-m=n;
-n=temp;
-gcd(m,n);
-}
-int power(int a, int b, int mod) 
-{ 
-int t; 
-if(b==1) 
- return a; 
-t=power(a,b/2,mod); 
-if(b%2==0) 
- return (t*t)%mod; 
-else 
- return (((t*t)%mod)*a)%mod; 
-} 
-void main()
-{
-int p,q;
-printf("enter the prime numbers ");
-scanf("%d%d",&p,&q);
-int m,n=p*q;
-printf("enter the message ");
-scanf("%d",&m);
-int x=(p-1)*(q-1);
-int e=2;
-while(e<x)
-{
- printf("s");
- if(gcd(e,x)==1)
- break;
- e ++;
-}//public key
-int d=2;
-while(d<x)
-{
- if((e*d)%x==1)
- break;
- d++;
-}//private key
-int enc=power(m,e,n);
-//encrypt
-int dec=power(enc,d,n);
-//decrypt
-printf("encryption: %d\n",enc);
-printf("decryption: %d\n",dec);
-}
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
+int checkPrime(int n)
+{
+    int i, m = n / 2;
+    for (i = 2; i <= m; i++)
+    {
+        if (n % i == 0)
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+int findGcd(int a, int b)
+{
+    int i, gcd;
+    for (i = 1; i <= a && i <= b; i++)
+    {
+        if (a % i == 0 && b % i == 0)
+        {
+            gcd = i;
+        }
+    }
+    return gcd;
+}
+int powMod(int a, int b, int n)
+{
+    long long int x = 1, y = a;
+    while (b > 0)
+    {
+        if (b % 2 == 1)
+            x = (x * y) % n;
+        y = (y * y) % n;
+        b = b / 2;
+    }
+    return x % n;
+}
+int main()
+{
+    int p, q;
+    int n, phin;
+    int data, cipher, decipher;
+    while (1)
+    {
+        printf("Enter two prime numbers: ");
+        scanf("%d %d", &p, &q);
+        if(!(checkPrime(p))&&(checkPrime(q))){
+            printf("Both are not prime\n");
+        }
+        else if (!checkPrime(p))
+        {
+            printf("p is not prime number\n");
+        }
+        else if (!checkPrime(q))
+        {
+            printf("q is not prime number\n");
+        }
+        else
+        {
+            break;
+        }
+    }
+    printf("p:%d and q:%d \n",p,q);
+    n = p * q;
+    phin = (p - 1) * (q - 1);
+    int e = 0;
+    for (e = 5; e <= 100; e++)
+    {
+        if (findGcd(phin, e) == 1)
+        {
+            break;
+        }
+    }
+    int d = 0;
+    for (d = e + 1; d <= 100; d++)
+    {
+        if ((d * e) % phin == 1)
+        {
+            break;
+        }
+    }
+    printf("value of e: %d\nvalue of d: %d\n", e, d);
+    printf("Enter the data to be encrypted: ");
+    scanf("%d", &data);
+    cipher = powMod(data, e, n);
+    printf("Encryption: %d\n", cipher);
+    decipher = powMod(cipher, d, n);
+    printf("Decryption: %d\n", decipher);
+    return 0;
+}
